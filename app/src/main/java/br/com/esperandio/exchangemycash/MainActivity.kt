48fb9f3b5b1f94f8419828b8e3b4247dcde68831
Emanuel <esperandioalmeida@gmail.com>
2023-08-19 00:31:04 -0300

    Adjusting redundant code

--- app/src/main/java/br/com/esperandio/exchangemycash/MainActivity.kt
+++ app/src/main/java/br/com/esperandio/exchangemycash/MainActivity.kt
@@ -2,21 +2,16 @@ package br.com.esperandio.exchangemycash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.LayoutRes
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.widget.doOnTextChanged
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
+import java.math.RoundingMode
 
 lateinit var tvResultMoney : TextView
 lateinit var etStartMoney : EditText
@@ -30,58 +25,13 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener  {
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
-
         spinnerTo = findViewById(R.id.spinner_to)
         etStartMoney = findViewById(R.id.et_mystartvalue)
         tvResultMoney = findViewById(R.id.tv_result)
         spinnerFrom = findViewById(R.id.spinner_from)
 
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.currencies,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinnerTo.adapter = adapter
-            spinnerFrom.adapter = adapter
-        }
-
-
         GlobalScope.launch {
             apiResult = RetrofitInstance.api.getRates()
-
-            var fromValue : Double = 0.0
-            var toValue: Double = 0.0
-
-            when(spinnerFrom.selectedItem){
-                "Australian Dollar"-> {fromValue = apiResult.rates.AUD}
-                "Real"-> {fromValue = apiResult.rates.BRL}
-                "Canadian Dollar"-> {fromValue = apiResult.rates.CAD}
-                "Swiss Franc"-> {fromValue = apiResult.rates.CHF}
-                "人民币"-> {fromValue = apiResult.rates.CNY}
-                "Euro"-> {fromValue = apiResult.rates.EUR}
-                "Pound Sterling"-> {fromValue = apiResult.rates.GBP}
-                "港幣"-> {fromValue = apiResult.rates.HKD}
-                "円"-> {fromValue = apiResult.rates.JPY}
-                "New Zealand Dollar"-> {fromValue = apiResult.rates.NZD}
-                "United States Dollar"-> {fromValue = apiResult.rates.USD}
-            }
-
-            when(spinnerTo.selectedItem){
-                "Australian Dollar"-> {toValue = apiResult.rates.AUD}
-                "Real"-> {toValue = apiResult.rates.BRL}
-                "Canadian Dollar"-> {toValue = apiResult.rates.CAD}
-                "Swiss Franc"-> {toValue = apiResult.rates.CHF}
-                "人民币"-> {toValue = apiResult.rates.CNY}
-                "Euro"-> {toValue = apiResult.rates.EUR}
-                "Pound Sterling"-> {toValue = apiResult.rates.GBP}
-                "港幣"-> {toValue = apiResult.rates.HKD}
-                "円"-> {toValue = apiResult.rates.JPY}
-                "New Zealand Dollar"-> {toValue = apiResult.rates.NZD}
-                "United States Dollar"-> {toValue = apiResult.rates.USD}
-            }
 
             spinnerFrom.onItemSelectedListener = this@MainActivity
             spinnerTo.onItemSelectedListener = this@MainActivity
@@ -126,7 +76,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener  {
         }
 
     }
-
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
@@ -173,7 +122,9 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener  {
         if(etStartMoney.text.isNullOrBlank() || etStartMoney.text.isEmpty()) {
             tvResultMoney.text = "0.0"
         }else{
-            tvResultMoney.text = calc(fromValue, toValue,etStartMoney.text.toString().toDouble()).toString()
+            var totalFinal = calc(fromValue, toValue,etStartMoney.text.toString().toDouble())
+            var df = totalFinal?.toBigDecimal()?.setScale(2, RoundingMode.HALF_EVEN)
+            tvResultMoney.text = df.toString()
         }
     }
 
